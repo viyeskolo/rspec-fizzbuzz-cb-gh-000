2729ed2cda74df1504bc256ee26c17a57e9fed6e
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-25 19:16:32 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,24 +1,9 @@
-require_relative './spec_helper.rb'
-
-def "fizzbuzz"
-  if fizzbuzz == (x) / 3
-    puts "Fizz"
-    fizz_3 = fizzbuzz(3)
-
-  end
-  it 'returns "Buzz" when the number is divisible by 5' do
-    fizz_5 = fizzbuzz(5)
-
-    expect(fizz_5).to eq("Buzz")
-  end
-  it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
-    fizz_15 = fizzbuzz(15)
-
-    expect(fizz_15).to eq("FizzBuzz")
-  end
-  it 'returns nil when the number is not divisible by 3 or 5' do
-    fizz_4 = fizzbuzz(4)
-
-    expect(fizz_4).to eq(nil)
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
   end
 end
+
+fizzbuzz(3) # => You should see a return of "Fizz"
+fizzbuzz(5) # => You should see a return of nil
+fizzbuzz()  # => You should get an ArgumentError
