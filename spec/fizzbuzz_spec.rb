f38792ba1ce2552208e9da2df0475e94d3f5710e
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-25 19:13:05 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,10 @@
 require_relative './spec_helper.rb'
 
-describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
+def "fizzbuzz"
+  if fizzbuzz == (x) / 3
+    puts "Fizz"
     fizz_3 = fizzbuzz(3)
 
-    expect(fizz_3).to eq("Fizz")
   end
   it 'returns "Buzz" when the number is divisible by 5' do
     fizz_5 = fizzbuzz(5)
