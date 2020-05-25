8f03ed3ac19d55739c637a024546dbe3cce9dfce
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-25 19:21:26 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,12 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
   puts "Fizz" # Go fizz
-  end
-end
 
-fizzbuzz(3) # => You should see a return of "Fizz"
-fizzbuzz(5) # => You should see a return of nil
-fizzbuzz()  # => You should get an ArgumentError
+elsif int % 5 == 0
+  "Buzz"
+
+elsif int () == 0
+  "error"
+  end
+  
 end
